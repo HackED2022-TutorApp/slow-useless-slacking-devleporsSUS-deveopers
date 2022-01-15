0cb433a2baf3931590307f11d2ccd7a859e528a0
Xuanhao Wang <67352673+ccBrita@users.noreply.github.com>
2022-01-15 13:55:39 -0700

    Created the tutor model

--- uatutor/api/models.py
+++ uatutor/api/models.py
@@ -1,3 +1,19 @@
+from math import degrees
 from django.db import models
 
-# Create your models here.
+class Tutors(models.Model):
+    FirstName = models.CharField(max_length=30)
+    LastName = models.CharField(max_length=20)
+    dob = models.DateField()
+    description = models.TextField(max_length=150)
+    degreeChoices =[
+        ('Ba/BSc.', "Bachelors"),
+        ('MSc', "Master"),
+        ('Phd', 'Doctoral')
+    ]
+    degree = models.CharField(choices=degreeChoices)
+    major = models.CharField(max_length=20, blank=False, null=False)
+    minor = models.CharField(max_length=20, blank =True, null=True)
+    subject = models.CharField()
+    
+
